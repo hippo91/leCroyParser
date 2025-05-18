6abe23b3a3be89bc6ef8d686f2276f22942be10a
Guillaume Peillex <guillaume.peillex@gmail.com>
2025-05-18 11:46:00 +0200

    [DEV] Adds test for parsing float32 value

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -1,6 +1,7 @@
 """
 Test the unpack function from parsing.py
 """
+import struct
 # mypy: ignore-errors
 from lecroyparser.parsing import unpack
 
@@ -74,3 +75,37 @@ def test_unpack_null_offset_null_position_str():
                    format_specifier="S6")
     assert result.decode() == expected_output, f"Expected {expected_output}, but got {result}"
 
+
+def test_unpack_null_offset_null_position_float():
+    """
+    Test the unpack function from scope_data_func.py
+
+    Offset and position are both 0
+    The input data is a 4-byte float
+    The expected output is the float value of the input data
+
+    The test checks both big-endian and little-endian formats
+    """
+    offset = 0
+    position = 0
+
+    expected_output = 2000.0
+    input_data = struct.pack(">f", expected_output)
+
+    # Test Big Endian
+    result = unpack(data=input_data,
+                   offset=offset,
+                   position=position,
+                   length=4,
+                   endianness=">",
+                   format_specifier="f4")
+    assert result == expected_output, f"Expected {expected_output}, but got {result}"
+
+    #  Test Little Endian
+    result = unpack(data=input_data[::-1],
+                   offset=offset,
+                   position=position,
+                   length=4,
+                   endianness="<",
+                   format_specifier="f4")
+    assert result == expected_output, f"Expected {expected_output}, but got {result}"
